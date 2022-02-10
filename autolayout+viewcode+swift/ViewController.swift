921fbda679da213854d96b078421702940580953
João Vitor Duarte Mariucio <joao.mariucio@mesainc.com.br>
2022-02-10 09:16:46 -0300

    Initial Commit

---
+++ autolayout+viewcode+swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autolayout+viewcode+swift
+//
+//  Created by João Vitor Duarte Mariucio on 10/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ autolayout+viewcode+swiftUITests/autolayout_viewcode_swiftUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  autolayout_viewcode_swiftUITestsLaunchTests.swift
+//  autolayout+viewcode+swiftUITests
+//
+//  Created by João Vitor Duarte Mariucio on 10/02/22.
+//
+
+import XCTest
+
+class autolayout_viewcode_swiftUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
